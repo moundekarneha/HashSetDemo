aba0d043ad7bdc54701a778c8148ee035b5c48c4
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 17:52:04 +0530

    Add project files.

---
+++ HashSetDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stud = new SortedSet<string>();  //HashSet
+            stud.Add("Alan");  
+            stud.Add("John");
+            stud.Add("John");
+            stud.Add("John");
+            foreach (var item in stud)
+            {
+                Console.WriteLine("Data = " + item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
